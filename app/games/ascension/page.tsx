6eef01a8b4ab38d6eff82fc034f7e95c15589ab8
sainoman15 <chamkhoat@gmail.com>
2026-03-27 01:15:03 +0700

    fix push ascention(1)

--- app/games/ascension/page.tsx
+++ app/games/ascension/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
-import App from './App.jsx';
+import App from './App';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+export default function AscensionPage() {
+  return (
+    <main>
+      <App />
+    </main>
+  );
+}
